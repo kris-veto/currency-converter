550166755a369af7643dc9f0dcc6d4faa9c8a0d8
kris-veto <82740207+kris-veto@users.noreply.github.com>
2024-08-11 04:07:41 -0400

    Update script.js

--- script.js
+++ script.js
@@ -9,6 +9,11 @@ const apiKey = '61b9302f9cc87cc7fc84504b';
 const convertButton = document.getElementById('convertButton'); 
 const swapButton = document.getElementById('swapButton');
 
+document.addEventListener('DOMContentLoaded', () => {
+    getImg1();
+    getImg2();
+});
+
 // Fetch the exchange rate  and convert when the button is clicked
 convertButton.addEventListener('click', () => {
     convertCurrency();
@@ -78,11 +83,11 @@ swapButton.addEventListener('click', () => {
 function getImg1() {
     const valorFromCurrency = document.getElementById('fromCurrency').value;
     const flagUrl = `https://www.xe.com/svgs/flags/${valorFromCurrency.toLowerCase()}.static.svg`;
-    document.getElementById('imgContenedor1').innerHTML = `<img src="${flagUrl}" alt="Flag of ${valorFromCurrency}" width="40" style="border: 1px solid rgb(191, 191, 191)">`;
+    document.getElementById('imgContenedor1').innerHTML = `<img src="${flagUrl}" alt="Flag of ${valorFromCurrency}" width="50" style="border: 1px solid rgb(191, 191, 191)">`;
 }
 
 function getImg2() {
     const valorToCurrency = document.getElementById('toCurrency').value;
     const flagUrl = `https://www.xe.com/svgs/flags/${valorToCurrency.toLowerCase()}.static.svg`;
-    document.getElementById('imgContenedor2').innerHTML = `<img src="${flagUrl}" alt="Flag of ${valorToCurrency}" width="40" style="border: 1px solid rgb(191, 191, 191)">`;
+    document.getElementById('imgContenedor2').innerHTML = `<img src="${flagUrl}" alt="Flag of ${valorToCurrency}" width="50" style="border: 1px solid rgb(191, 191, 191)">`;
 }
